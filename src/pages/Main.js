d273d85d5037715b634c8fe82ce59964cc62d585
dbowers <dbowers@vestmark.com>
2019-09-24 17:38:07 -0400

    multiple pages and media selection

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import LoginButton from './modules/LoginButton';
 import TokenInput from './modules/TokenInput';
 import './css/Main.css';
+// Remind me to never try to write an entire single-page in a literal (mostly) single page again
+//                                                                                                        im sorry for your eyes
 
 /*
     TODO LIST
-  ! Handler for empty returns on search
+  ! none
 */
 
 const width = 500;
@@ -25,6 +27,9 @@ export default class Main extends Component {
       type: '',
       search: '',
       searchResults: [],
+      cachedSearchResults: {},
+      page: 1,
+      selectedMedia: {},
     };
     this.noResultsFoundResponse = [
       {
@@ -86,11 +91,11 @@ export default class Main extends Component {
           <span>Press </span>
           <span className="aom-a bold">Enter</span>
           <span> to submit your search, </span>
-          <span className="green bold">T</span>
+          <span className="green bold">↑</span>
           <span> to toggle titles, </span>
           <span className="cyan bold">← →</span>
           <span> to change pages or </span>
-          <span className="aom-m bold">1 through 4</span>
+          <span className="aom-m bold">F1 through F4</span>
           <span> to select the media</span>
         </span>
       ),
@@ -108,13 +113,19 @@ export default class Main extends Component {
     }
     // Response was successful, we now know the token is legitimate. Time to begin the real heavy-lifting.
     this.setState({ loginState: 'leaving' });
-    setTimeout(() => this.setState({ loginState: 'left', mainState: 'preenter' }), 750);
-    setTimeout(() => this.setState({ mainState: 'entering', substate: 'a-or-m-phase' }), 752);
-  }
+    setTimeout(
+      () => this.setState({ loginState: 'left', mainState: 'preenter' }),
+      750,
+    );
+    setTimeout(
+      () => this.setState({ mainState: 'entering', substate: 'a-or-m-phase' }),
+      752,
+    );
+  };
 
   handleVerifyError = (e) => {
     console.log(e);
-  }
+  };
 
   authorizeToken = (token) => {
     this.setState({ submitDisabled: true });
@@ -128,20 +139,27 @@ export default class Main extends Component {
       }
     }`;
     const options = this.generateQueryJson(query, token);
-    console.log(options);
+    // console.log(options);
 
     this.setState({ token });
-    fetch(ANILIST_BASE_URL, options)
-      .then(this.handleVerifyResponse, this.handleVerifyError);
-  }
+    fetch(ANILIST_BASE_URL, options).then(
+      this.handleVerifyResponse,
+      this.handleVerifyError,
+    );
+  };
 
   transitionMainState = async (nextState) => {
     const { substate } = this.state;
 
     const oldPhaseElement = document.getElementById(substate);
     const newPhaseElement = document.getElementById(nextState);
-    if (!oldPhaseElement || !newPhaseElement) { // Something went wrong, but we don't want to explode so just halt
-      console.log('Unable to find one of the phase elements:', oldPhaseElement, newPhaseElement);
+    if (!oldPhaseElement || !newPhaseElement) {
+      // Something went wrong, but we don't want to explode so just halt
+      console.log(
+        'Unable to find one of the phase elements:',
+        oldPhaseElement,
+        newPhaseElement,
+      );
       return;
     }
 
@@ -155,7 +173,8 @@ export default class Main extends Component {
       newPhaseElement.classList.replace('inactive', 'preenter');
       setTimeout(() => {
         newPhaseElement.classList.replace('preenter', 'active');
-        setTimeout(() => { // Focus the element after we're sure it's on page
+        setTimeout(() => {
+          // Focus the element after we're sure it's on page
           const nextStartingElement = this.startingElementIdByPhase[nextState];
           if (nextStartingElement) {
             document.getElementById(nextStartingElement).focus();
@@ -164,12 +183,13 @@ export default class Main extends Component {
         }, 250);
       }, 10);
     }, 740);
-  }
+  };
 
   handleKeyPress = (e) => {
     const {
       substate, titleShowState, search, type,
     } = this.state;
+    console.log(e.key);
     switch (substate) {
       case 'a-or-m-phase':
         if (e.key === 'a') {
@@ -182,41 +202,120 @@ export default class Main extends Component {
         }
         break;
       case 'search-phase':
-        // console.log(e.key)
-        if (e.key === 't') {
-          this.setState({ titleShowState: !titleShowState });
-        }
-        if (e.key === 'Enter') {
-          // console.log(this.state.search, this.state.type)
-          const query = `query {
-            Page(page: 1, perPage: 4) {
-              media(search: "${search}", type: ${type}) {
-                id
-                title {
-                  userPreferred
-                }
-                coverImage {
-                  large
-                  color
-                }
+        const searchBaseQuery = (newPage) => `query {
+          Page(page: ${newPage}, perPage: 4) {
+            pageInfo {
+              total
+            }
+            media(search: "${search}", type: ${type}) {
+              id
+              title {
+                userPreferred
+              }
+              coverImage {
+                large
+                color
               }
             }
-          }`;
-          console.log(query);
-          const options = this.generateQueryJson(query);
-          fetch(ANILIST_BASE_URL, options)
-            .then((resp) => resp.json())
-            .then((resp) => {
-              if (resp.data.Page.media.length === 0) {
-                this.setState({ searchResults: this.noResultsFoundResponse });
-              } else {
-                this.setState({ searchResults: resp.data.Page.media || this.noResultsFoundResponse });
-              }
-            });
+          }
+        }`;
+        switch (e.key) {
+          case 'F1':
+            this.selectMediaIndex(0);
+            e.preventDefault();
+            break;
+          case 'F2':
+            this.selectMediaIndex(1);
+            e.preventDefault();
+            break;
+          case 'F3':
+            this.selectMediaIndex(2);
+            e.preventDefault();
+            break;
+          case 'F4':
+            this.selectMediaIndex(3);
+            e.preventDefault();
+            break;
+          case 'ArrowUp':
+            this.setState({ titleShowState: !titleShowState });
+            break;
+          case 'ArrowLeft':
+            this.changeSearchPage(-1, searchBaseQuery);
+            e.preventDefault();
+            break;
+          case 'ArrowRight':
+            this.changeSearchPage(1, searchBaseQuery);
+            e.preventDefault();
+            break;
+          case 'Enter':
+            // Reset our search values back to nothing when performing a fresh search
+            this.setState({ page: 1, cachedSearchResults: {} });
+            const options = this.generateQueryJson(searchBaseQuery(1));
+            fetch(ANILIST_BASE_URL, options)
+              .then((resp) => resp.json())
+              .then((resp) => {
+                console.log(resp);
+                if (resp.data.Page.media.length === 0) {
+                  this.setState({ searchResults: this.noResultsFoundResponse });
+                } else {
+                  this.setState({
+                    searchResults: resp.data.Page.media || this.noResultsFoundResponse,
+                    searchPages: Math.ceil(resp.data.Page.pageInfo.total / 4),
+                  });
+                }
+              });
+            break;
+          default:
+            document.getElementById('search-input').focus();
+            break;
         }
         break;
       default:
         break;
+    }
+  };
+
+  selectMediaIndex = (index) => {
+    const { searchResults } = this.state;
+    if (Object.keys(searchResults[index]).length === 0) {
+      return; // Likely we tried to select something that isnt present on the last page of the search
+    }
+    this.setState({ selectedMedia: searchResults[index] });
+    this.transitionMainState('data-phase');
+  };
+
+
+  changeSearchPage = (direction, baseQueryCallback) => {
+    const { cachedSearchResults, page, searchPages } = this.state;
+    if (page + direction < 1 || page + direction > searchPages) return; // No page 0s or extra queries :)
+
+    this.setState({ page: page + direction });
+    const query = baseQueryCallback(page + direction);
+
+    if (cachedSearchResults[page + direction]) {
+      console.log('Cached search results found, using those instead');
+      this.setState({
+        searchResults: cachedSearchResults[page + direction],
+      });
+    } else {
+      const options = this.generateQueryJson(query);
+      fetch(ANILIST_BASE_URL, options)
+        .then((resp) => resp.json())
+        .then((resp) => {
+          if (resp.data.Page.media.length === 0) {
+            // If no results, default to your "no results" set
+            this.setState({ searchResults: this.noResultsFoundResponse });
+          } else {
+            this.setState({
+              searchResults:
+                  resp.data.Page.media || this.noResultsFoundResponse,
+              cachedSearchResults: {
+                ...cachedSearchResults,
+                [page + direction]: resp.data.Page.media,
+              },
+            });
+          }
+        });
     }
   }
 
@@ -233,11 +332,19 @@ export default class Main extends Component {
         query,
       }),
     };
-  }
+  };
 
   render() {
     const {
-      loginState, submitDisabled, mainState, substate, search, searchResults, titleShowState, lastSubstate,
+      loginState,
+      submitDisabled,
+      mainState,
+      substate,
+      search,
+      searchResults,
+      titleShowState,
+      lastSubstate,
+      selectedMedia,
     } = this.state;
     return (
       <div id="app">
@@ -262,7 +369,7 @@ export default class Main extends Component {
         </div>
 
         <div id="help-message" className={substate}>
-          { this.helpByPhase[substate] || this.helpByPhase[lastSubstate] }
+          {this.helpByPhase[substate] || this.helpByPhase[lastSubstate]}
         </div>
         <div id="main-phase" className={mainState}>
           <div id="a-or-m-phase" className="main-phase-item active">
@@ -284,7 +391,12 @@ export default class Main extends Component {
               value={search}
               onChange={(e) => this.setState({ search: e.target.value })}
             />
-            <div id="results-view" className={`${searchResults.length === 0 ? 'inactive' : 'active'}`}>
+            <div
+              id="results-view"
+              className={`${
+                searchResults.length === 0 ? 'inactive' : 'active'
+              }`}
+            >
               {searchResults.map((work) => (
                 <div id="results-item" key={work.id}>
                   {titleShowState ? (
@@ -296,18 +408,30 @@ export default class Main extends Component {
                     >
                       {work.title.userPreferred}
                     </div>
-                  ) : (<div />)}
+                  ) : (
+                    <div />
+                  )}
                   <div
                     className="img"
                     style={{
-                    // backgroundImage: `url('${work.coverImage.large}')`
+                      // backgroundImage: `url('${work.coverImage.large}')`
                       border: `1px solid ${work.coverImage.color}`,
-                      backgroundColor: `#${(`${Math.floor(Math.random() * 9)}`).repeat(6)}`,
+                      backgroundColor: `#${`${Math.floor(
+                        Math.random() * 9,
+                      )}`.repeat(6)}`,
                     }}
                   />
                 </div>
               ))}
             </div>
+          </div>
+          <div id="data-phase" className="main-phase-item inactive">
+            {substate === 'data-phase' ? (
+              <div>
+                aa
+                {selectedMedia.id}
+              </div>
+            ) : <div />}
           </div>
         </div>
       </div>
